acad924ee3f24d5e252cf29227ef852c437f8311
abid <abid@michaels.com>
2023-08-15 12:09:52 -0500

    finished coding quiz app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.css'
 
-function App() {
+const TILE_COLORS = ['red', 'green', 'blue', 'yellow'];
+
+export default function Memory() {
+  // Write your code here.
+
+  // shuffle the board with 8 tiles
+  // board = [red, yellow, blue, ...]
+  const [board, setBoard] = useState(()=>{
+    return shuffle([...TILE_COLORS, ...TILE_COLORS]);
+  });
+
+  // tracking tiles that are selected
+  const [selectedTiles, setSelectedTiles] = useState([]);
+
+  // tracking tiles that are matched
+  const [matchedTiles, setMatchedTiles] = useState([]);
+
+
+  const selectTiles = (i) => {
+    if (selectedTiles.length >= 2 || selectedTiles.includes(i) || matchedTiles.includes(i)) return;
+    setSelectedTiles([...selectedTiles, i])
+  }
+
+
+  useEffect(() => {
+    // if less than 2 tiles are selected simply return
+    if (selectedTiles.length < 2) return;
+    // set matchedTiles if both tiles are same color
+    if (board[selectedTiles[0]] === board[selectedTiles[1]]){
+      setMatchedTiles([...matchedTiles, ...selectedTiles]);
+      // empty the selectedTiles once done
+      // this will allow you to also select tiles when you have already found 2 matching tiles
+      setSelectedTiles([]);
+    }
+    // if they are not the same they should deselect after 1 sec
+    // they will remain selected if they match
+    else {
+      const timeoutId = setTimeout(()=> {setSelectedTiles([]);}, 1000);
+      return ()=> clearTimeout(timeoutId);
+    }
+  }, [selectedTiles]);
+
+  const didPlayerWin = matchedTiles.length === board.length;
+
+  // you need to reset all the states instead of just matched tiles to 
+  // reshuffle the board as well
+  const restartGame = () => {
+    setBoard(shuffle([...TILE_COLORS, ...TILE_COLORS]));
+    setMatchedTiles([]);
+    setSelectedTiles([]);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* Write your code here. */}
+      <h1>{didPlayerWin ? `You Win!` : `Memory`}</h1>
+      <div className='board'>
+        {board.map((tileColor, i)=>{
+          const isTurnedOver = selectedTiles.includes(i) || matchedTiles.includes(i);
+          const className = isTurnedOver ? `tile ${tileColor}` : `tile`;
+          return(
+            <div key={i} className={className} onClick={()=>{selectTiles(i)}}/>
+          );
+        })}
+      </div>
+      {/* we want to show the restart button only when the player has won */}
+      {/* short circuit expression */}
+      {didPlayerWin && <button onClick={()=>restartGame()}>Restart</button>}
+    </>
   );
 }
 
-export default App;
+/**
+ * Returns the array shuffled into a random order.
+ * Do not edit this function.
+ */
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const randomIndex = Math.floor(Math.random() * (i + 1));
+
+    // Swap the elements at i and randomIndex
+    [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
+  }
+  return array;
+}
